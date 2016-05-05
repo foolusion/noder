632cfecc2987b2397398a7c9b1d256afdb821424
Andrew O'Neill <andrew.oneill@nordstrom.com>
2016-05-04 22:23:05 -0700

    add js print and aws upload

--- main.go
+++ main.go
@@ -13,15 +13,25 @@ import (
 	"unicode"
 	"unicode/utf8"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+
 	"golang.org/x/net/html"
 )
 
+var config = struct {
+	region string
+	bucket string
+}{}
+
 func main() {
-	run("http://shop.nordstrom.com/c/women")
-	run("http://shop.nordstrom.com/c/men")
+	var ih *imageHelper
+	run("http://shop.nordstrom.com/c/women", ih)
+	run("http://shop.nordstrom.com/c/men", ih)
 }
 
-func run(url string) {
+func run(url string, ih *imageHelper) {
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "noder: %v\n", err)
@@ -33,16 +43,19 @@ func run(url string) {
 		os.Exit(1)
 	}
 
-	ih := &imageHelper{
-		baseURL:         resp.Request.URL,
-		urlToHash:       make(map[string]string, 20),
-		imageDownloaded: make(map[string]struct{}, 20),
+	if ih == nil {
+		ih = &imageHelper{
+			urlToHash:       make(map[string]string, 20),
+			imageDownloaded: make(map[string]struct{}, 20),
+		}
 	}
 
 	node := getNodeByID(doc, "main-content")
+	ih.baseURL = resp.Request.URL
 	forEachNode(node, ih.downloadImages)
 	forEachNode(node, stripCommentAndSpace)
-	html.Render(os.Stdout, node)
+	js := &jsHelper{}
+	forEachNode(node, js.nodeToJs)
 }
 
 func forEachNode(n *html.Node, f func(*html.Node) bool) {
@@ -106,6 +119,24 @@ func stripCommentAndSpace(n *html.Node) bool {
 	return false
 }
 
+type jsHelper struct {
+	tileNum int
+}
+
+func (js *jsHelper) nodeToJs(n *html.Node) bool {
+	for _, v := range n.Attr {
+		switch {
+		case v.Key == "class" && strings.Contains(v.Val, "story-tile"):
+			js.tileNum++
+			var buf bytes.Buffer
+			html.Render(&buf, n)
+			fmt.Fprintf(os.Stdout, "var t%[1]v = document.querySelector('#main-content .story-tile:nth-of-type(%[1]v)');\nt%[1]v.innerHTML = %q;\n\n", js.tileNum, buf.String())
+			return true
+		}
+	}
+	return false
+}
+
 type imageHelper struct {
 	baseURL         *url.URL
 	urlToHash       map[string]string
@@ -116,7 +147,7 @@ func (ih *imageHelper) downloadImages(n *html.Node) bool {
 	if n.Type != html.ElementNode || n.Data != "img" {
 		return false
 	}
-	for _, a := range n.Attr {
+	for i, a := range n.Attr {
 		if a.Key != "src" {
 			continue
 		}
@@ -126,7 +157,7 @@ func (ih *imageHelper) downloadImages(n *html.Node) bool {
 			continue
 		}
 		name := ih.downloadImage(u.String())
-		n.Data = name
+		n.Attr[i].Val = name
 	}
 	return false
 }
@@ -159,21 +190,41 @@ func (ih *imageHelper) downloadImage(imgURL string) string {
 
 	// save url to hash mapping
 	filename := fmt.Sprintf("%x%v", h.Sum(nil), ext)
-	ih.urlToHash[imgURL] = filename
+	url := "https://images.nordstromdata.com/cc/img/" + filename
+	ih.urlToHash[imgURL] = url
 
 	// if content was downloaded from a different url don't save a new file
 	if _, ok := ih.imageDownloaded[filename]; ok {
 		return filename
 	}
 
-	f, err := os.Create(filename)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "downloadImage: opening file: %v\n", err)
-		return ""
-	}
-	defer f.Close()
+	var contentType string
 
-	io.Copy(f, &buf)
+	switch ext {
+	case ".jpg", ".jpeg":
+		contentType = "image/jpeg"
+	case ".png":
+		contentType = "image/png"
+	case ".gif":
+		contentType = "image/gif"
+	}
+
+	sess := session.New(&aws.Config{Region: aws.String(config.region)})
+
+	svc := s3.New(sess)
+	params := &s3.PutObjectInput{
+		Bucket:      aws.String(config.bucket),
+		Key:         aws.String("cc/img/" + filename),
+		Body:        bytes.NewReader(buf.Bytes()),
+		ContentType: aws.String(contentType),
+		GrantRead:   aws.String("Everyone"),
+	}
+
+	_, err = svc.PutObject(params)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "downloadImage: uploading to s3: %v\n", err)
+	}
+
 	ih.imageDownloaded[filename] = struct{}{}
 	return filename
 }
